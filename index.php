03b2309378159507fb8502c11cb7cbc3780987b9
farhan3107 <farhangrn31@gmail.com>
2020-04-02 18:14:56 +0700

    Menampilkan data database dalam bentuk grafik

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
+<?php
+$koneksi    = mysqli_connect("localhost", "root", "", "grafik");
+$penjualan  = mysqli_query($koneksi, "SELECT penjualan FROM sales order by ID asc");
+$merk       = mysqli_query($koneksi, "SELECT merk FROM sales order by ID asc");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -329,48 +334,52 @@
         </nav>
         <!-- End of Topbar -->
 
-        <h2>Grafik Grafis/Line Menggunakan chartjs</h2>
-<canvas id="grafikBatang"></canvas>
+        <!-- Main Content -->
+        <canvas id="barchart"></canvas>
+        <script  type="text/javascript">
+  var ctx = document.getElementById("barchart").getContext("2d");
+  var data = {
+            labels: [<?php while ($p = mysqli_fetch_array($merk)) { echo '"' . $p['merk'] . '",';}?>],
+            datasets: [
+            {
+              label: "Penjualan Barang",
+              data: [<?php while ($p = mysqli_fetch_array($penjualan)) { echo '"' . $p['penjualan'] . '",';}?>],
+              backgroundColor: [
+                '#29B0D0',
+                '#2A516E',
+                '#F07124',
+                '#CBE0E3',
+                '#979193'
+              ]
+            }
+            ]
+            };
 
-<script>
-var ctx = document.getElementById("grafikBatang").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
+  var myBarChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: {
+            legend: {
+              display: false
+            },
+            barValueSpacing: 20,
+            scales: {
+              yAxes: [{
+                  ticks: {
+                      min: 0,
+                  }
+              }],
+              xAxes: [{
+                          gridLines: {
+                              color: "rgba(0, 0, 0, 0)",
+                          }
+                      }]
+              }
+          }
+        });
 </script>
+        <!-- End Content-->
+
         <!-- Bootstrap core JavaScript-->
   <script src="asset/vendor/jquery/jquery.min.js"></script>
   <script src="asset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
